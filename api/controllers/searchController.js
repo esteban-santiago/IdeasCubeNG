61fb6f465a8095de9c9b825de90385d12f67ded5
Esteban Santiago <esteban_santiago@MBP-de-Esteban.cinema.montreuil.wan.bsf-intranet.org>
2018-09-20 12:29:04 +0200

    Not implemented functions

--- api/controllers/searchController.js
+++ api/controllers/searchController.js
@@ -6,20 +6,24 @@ const PATH = '/api/ideascubeng/v1.0';
 
 module.exports = function(app) {
 
-  app.get(PATH + '/search', function(req, res){
-
-  });
 
   app.get(PATH + '/search/:keyword', function(req, res){
     searchService.search(req.params.keyword)
       .then((results) => res.send(results));
   });
 
+  //Not Implemented
   app.get(PATH + '/search/:field/:keyword', function(req, res){
     searchService.search(req.params.keyword)
       .then((results) => res.send(results));
   });
 
+  //Not Implemented
+  app.get(PATH + '/search', function(req, res){
+
+  });
+
+  //Not Implemented
   app.get(PATH + '/search/:language/:field/:keyword', function(req, res){
     searchService.search(req.params.keyword)
       .then((results) => res.send(results));
