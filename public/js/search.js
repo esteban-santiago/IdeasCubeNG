65de8f509dd839f89b60e132997c42efa144e4a3
Esteban Santiago <esteban_santiago@MacBook-Pro-de-Esteban.local>
2019-03-28 16:38:34 +0100

    Bug: searh.js

--- public/js/search.js
+++ public/js/search.js
@@ -2,7 +2,7 @@ $("#searchb").click(function() {
   $('#content_box').html('');
   $.ajax({
     type: "get",
-    url: "/api/ideascubeng/v1.0/search/" + $('#keywords').val()
+    url: "/api/mediacenter/v1.0/search/" + $('#keywords').val()
   }).then(function(data) {
     data.forEach(function(entry) {
       $('#content_box').append('<a href="' + entry.href + '">' + entry.title + '</a><br>');
